1cfca40683768d6a93574b3f23650687f4bd913e
LabibIslam <labib.islam.practice@gmail.com>
2022-03-23 15:56:03 +0600

    completed lesson 45-3

--- src/App.js
+++ src/App.js
@@ -13,32 +13,34 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude. React. How are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222 + number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p style={{color:'blue', backgroundColor: 'yellow'}}>Name: {singer2.name} {singer2.job}</p>
-
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Perosn></Perosn>
+      <Perosn></Perosn>
+      <Perosn></Perosn>
+      <Perosn></Perosn>
+      <h2>New Component. yaaaah</h2>
+      <Friends></Friends>
+      <Friends></Friends>
+      <Friends></Friends>
     </div>
   );
+}
+
+function Perosn(){
+  return (
+    <div className='perosn'>
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: Cricketer</p>
+  </div>
+  )
+}
+
+function Friends(){
+  return(
+    <div className='container'>
+      <h3>Name: Ajay Devgun</h3>
+      <p>Job: Maramari</p>
+    </div>
+  )
 }
 
 export default App;
